a421b8e1b53ed5844adeb1b35fdf2812fa109ef6
Wes Hendley <wrhendley@gmail.com>
2023-07-12 15:39:06 -0500

    Add #column_is_full?

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ require_relative 'player'
 
 class Board
   attr_reader :player1, :player2, :grid
-  
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
@@ -11,6 +11,14 @@ class Board
 
   def add_token(column_number, color)
     empty_slot_index = @grid.transpose[column_number].find_index(nil)
-    grid[empty_slot_index][column_number] = color
+    if empty_slot_index && empty_slot_index <= 5
+      @grid[empty_slot_index][column_number] = color
+    else
+      puts 'Column number 0 is full, please play in another column'
+    end
+  end
+
+  def column_is_full?(column_number)
+    @grid.transpose[column_number].none?(nil)
   end
 end
