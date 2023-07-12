5d2d628f46698513ea0e62cfb016c4518ea2d5dd
Wes Hendley <wrhendley@gmail.com>
2023-07-12 17:12:32 -0500

    Update #play so that it calls #add_token with the correct number of arguments

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,14 @@
 require_relative 'board'
 
 class Player
+  attr_reader :name, :color
+
   def initialize(name, color)
     @name = name
     @color = color
   end
 
   def play(board, column_number)
-    board.add_token(column_number)
+    board.add_token(column_number, @color)
   end
 end
