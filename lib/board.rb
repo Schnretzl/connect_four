7ec5b810120335cd5507378acb98603ab00f3ca8
Wes Hendley <wrhendley@gmail.com>
2023-07-12 10:56:14 -0500

    Add #add_token

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,16 @@
 require_relative 'player'
 
 class Board
-  def initialize
+  attr_reader :player1, :player2, :grid
+  
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
     @grid = Array.new(6) { Array.new(7) }
+  end
+
+  def add_token(column_number, color)
+    empty_slot_index = @grid.transpose[column_number].find_index(nil)
+    grid[empty_slot_index][column_number] = color
   end
 end
