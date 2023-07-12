a03ebdc114e8a72f72efda556e2f2b100ba73935
Wes Hendley <wrhendley@gmail.com>
2023-07-12 17:08:37 -0500

    Add #winner?, #column_winner?, #row_winner?, and #diagonal_winner?

--- lib/board.rb
+++ lib/board.rb
@@ -11,14 +11,67 @@ class Board
 
   def add_token(column_number, color)
     empty_slot_index = @grid.transpose[column_number].find_index(nil)
-    if empty_slot_index && empty_slot_index <= 5
-      @grid[empty_slot_index][column_number] = color
-    else
+    if column_is_full?(column_number)
       puts 'Column number 0 is full, please play in another column'
+    else
+      @grid[empty_slot_index][column_number] = color
     end
   end
 
   def column_is_full?(column_number)
     @grid.transpose[column_number].none?(nil)
   end
+
+  def winner?
+    column_win = column_winner?
+    row_win = row_winner?
+    diagonal_win = diagonal_winner?
+    column_win || row_win || diagonal_win
+  end
+
+  private
+
+  def column_winner?
+    @grid.transpose.each do |column|
+      column.each_cons(4) do |group|
+        return true if group.compact.uniq.length == 1 && !group.any?(nil)
+      end
+    end
+
+    false
+  end
+
+  def row_winner?
+    @grid.each do |row|
+      row.each_cons(4) do |group|
+        return true if group.compact.uniq.length == 1 && !group.any?(nil)
+      end
+    end
+
+    false
+  end
+
+  def diagonal_winner?
+    2.times do |start_row|
+      3.times do |start_col|
+        diagonal = [
+          @grid[start_row][start_col..start_col + 3],
+          @grid[start_row + 1][start_col..start_col + 3],
+          @grid[start_row + 2][start_col..start_col + 3],
+          @grid[start_row + 3][start_col..start_col + 3]
+        ]
+        return true if diagonal.flatten.compact.uniq.length == 1
+      end
+    end
+
+    false
+  end
 end
+  # [
+  #   [0, 1, 2, 3, 4, 5, 6]
+  #   [0, 1, 2, 3, 4, 5, 6]
+  #   [0, 1, 2, 3, 4, 5, 6]
+  #   [0, 1, 2, 3, 4, 5, 6]
+  #   [0, 1, 2, 3, 4, 5, 6]
+  #   [0, 1, 2, 3, 4, 5, 6]
+  # ]
