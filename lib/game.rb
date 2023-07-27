419450541c9cfa6aa2f953156c7fbface94905b4
Wes Hendley <wrhendley@gmail.com>
2023-07-27 16:09:48 -0500

    Add #input_valid_color

--- lib/game.rb
+++ lib/game.rb
@@ -23,7 +23,37 @@ def input_valid_player_name(player_number)
   end
 end
 
-# player1_name = input_valid_player_name(1)
-# verify_input(player1_name)
-# player2_name = input_valid_player_name(2)
-# verify_input(player2_name)
+def input_valid_color(taken_colors = '')
+  loop do
+    puts 'What color do you want to play as?'
+    color = STDIN.gets.chomp
+    if taken_colors.include?(color)
+      puts "#{color.capitalize} has already been chosen, please pick another color."
+    else
+      begin
+        Rainbow('Valid color check').bg(color.to_sym)
+        return color
+      rescue ArgumentError
+        puts 'Invalid color entered or not a color.  Please enter a valid color.'
+      end
+    end
+  end
+end
+
+p1 = Player.new(input_valid_player_name(1), input_valid_color)
+p2 = Player.new(input_valid_player_name(2), input_valid_color(p1.color))
+game_board = Board.new(p1, p2)
+
+loop do
+  game_board.print_board
+  game_board.add_token(game_board.prompt_for_play_column)
+  if game_board.winner?
+    game_board.congratulate_winner
+    break
+  end
+  if game_board.board_is_full?
+    game_board.print_game_draw
+    break
+  end
+  game_board.change_current_player_turn
+end
